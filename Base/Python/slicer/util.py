b67f94398afc1c4a4da1f016095667eaa1d7f08e
Robert Prueckl <rprueckl@gmail.com>
2021-10-18 13:22:36 +0200

    ENH: pointed the python part to the correct main window object name

--- Base/Python/slicer/util.py
+++ Base/Python/slicer/util.py
@@ -184,7 +184,7 @@ def lookupTopLevelWidget(objectName, verbose = True):
   return None
 
 def mainWindow(verbose = True):
-  return lookupTopLevelWidget('qSlicerMainWindow', verbose)
+  return lookupTopLevelWidget('CORTEXPLORER_MAIN_WINDOW', verbose)
 
 def pythonShell(verbose = True):
   from slicer import app
